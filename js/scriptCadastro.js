ad23c3e1e4b6f0e83b45989cb599a56f5ec09188
Vini-Yking <mvdsfg@gmail.com>
2022-05-12 17:23:45 -0300

    ajuste no cadastro

--- js/scriptCadastro.js
+++ js/scriptCadastro.js
@@ -6,6 +6,7 @@ const testevalidado = () => {
 
   label = document.querySelectorAll(".obrigatorio")[3];
   label.style.color = "red";
+  label.textContent = label.textContent.toUpperCase();
   labelnome = document.querySelectorAll(".field")[3];
   labelnome.style.color = "red";
 };
@@ -18,7 +19,9 @@ const teste = () => {
 
   label = document.querySelectorAll(".obrigatorio")[3];
   label.style.color = "";
+  label.textContent = "* senha coincide";
   labelnome = document.querySelectorAll(".field")[3];
+
   labelnome.style.color = "";
 };
 
@@ -38,19 +41,29 @@ function check() {
   var senha1 = document.querySelector("#senha1").value;
   var senha2 = document.querySelector("#senha2").value;
 
-  if(nome !== "" && email !== "" && senha1 !== "" && senha2 !=="" && senha1 === senha2)
+  if (
+    nome !== "" &&
+    email !== "" &&
+    senha1 !== "" &&
+    senha2 !== "" &&
+    senha1 === senha2
+  )
     return true;
-  else{
+  else {
     return false;
   }
 }
 
-
 function msg() {
-  if(check())
+  if (check()) {
     alert("Cadastro Feito!");
-  else{
-    alert("Faltam dados obrigatórios.")
+  } else {
+    for (i = 0; i < 4; i++) {
+      document.querySelectorAll(".obrigatorio")[i].textContent =
+        "* Cheque esse campo";
+      document.querySelectorAll(".obrigatorio")[i].style.color = "red";
+    }
+
+    alert("Faltam dados obrigatórios.");
   }
 }
-
