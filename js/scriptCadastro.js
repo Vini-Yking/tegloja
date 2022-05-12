baa22a1292afb543a431c0d1086f37c283a97f08
Vini-Yking <mvdsfg@gmail.com>
2022-05-12 01:26:33 -0300

    informado no JS

--- js/scriptCadastro.js
+++ js/scriptCadastro.js
@@ -1,9 +1,33 @@
-function validarSenha(psw1, psw2)
-{
-   var senha1 = document.getElementById('senha1').value;
-   var senha2 = document.getElementById('senha2').value;
-    if (senha1 != "" && senha2 != "" && senha1 !== senha2)
-    {
-    	alert('senhas diferentes');
-    }
-}
+const testevalidado = () => {
+  senha2cor = document.querySelector("#senha2");
+  senha2cor.style.border = "1px solid red";
+
+  senha2cor.style.transition = "800ms";
+
+  label = document.querySelectorAll(".obrigatorio")[3];
+  label.style.color = "red";
+  labelnome = document.querySelectorAll(".field")[3];
+  labelnome.style.color = "red";
+};
+
+const teste = () => {
+  senha2cor = document.querySelector("#senha2");
+  senha2cor.style.border = "";
+
+  senha2cor.style.transition = "800ms";
+
+  label = document.querySelectorAll(".obrigatorio")[3];
+  label.style.color = "";
+  labelnome = document.querySelectorAll(".field")[3];
+  labelnome.style.color = "";
+};
+
+const validarSenha = (psw1, psw2) => {
+  var senha1 = document.querySelector("#senha1").value;
+  var senha2 = document.querySelector("#senha2").value;
+  if (senha1 != "" && senha2 != "" && senha1 !== senha2) {
+    testevalidado();
+  } else {
+    teste();
+  }
+};
