0b2ac090e8668f59b870360449d05e5f3808d6e4
Vini-Yking <mvdsfg@gmail.com>
2022-05-12 10:54:22 -0300

    ajuste na navegacao da pagina

--- js/scriptNavegacao.js
+++ js/scriptNavegacao.js
@@ -30,16 +30,16 @@ const navegaPrev = () => {
 
   switch (local) {
     case "/index02.html":
-      document.querySelector(".next").href = `${origem}/index.html`;
-      break;
-    case "/index02.html":
-      document.querySelector(".next").href = `${origem}/index03.html`;
+      document.querySelector(".previos").href = `${origem}/index.html`;
       break;
     case "/index03.html":
-      document.querySelector(".next").href = `${origem}/index04.html`;
+      document.querySelector(".previos").href = `${origem}/index02.html`;
+      break;
+    case "/index04.html":
+      document.querySelector(".previos").href = `${origem}/index03.html`;
       break;
 
     default:
-      document.querySelector(".next").href = `${origem}/index.html`;
+      document.querySelector(".previos").href = `${origem}/index04.html`;
   }
 };
